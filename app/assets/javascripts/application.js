e86cdf6e6afcbd537f1f67d0fe5fa0a2d702430d
kimjohnconcha <kimjohnconcha@icloud.com>
2019-06-17 11:26:13 +0800

    Finish JS, etc.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,14 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+
+
+message_appender = function(content) {
+  $('#messages-table').append(content);
+}
+
+$(document).on('turbolinks:load', function() {
+  message_appender('hello, world!');
+});
